b33435b54605e0a06d73fe899f5f542cb1ec8051
Zain Latif <zainlatif702@gmail.com>
2025-05-23 10:53:40 +0500

    remove some minor error

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
@@ -9,19 +9,20 @@ app.use(cors());
 app.use(express.json());
 
 // Import product routes
-const productRoutes = require('./routes/productRoutes');
-app.use('/api/products', productRoutes);
+const productRoutes = require("./routes/productRoutes");
+app.use("/api/products", productRoutes);
 
 // Connect to MongoDB and start server
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('✅ MongoDB connected');
+    console.log("✅ MongoDB connected");
     app.listen(process.env.PORT, () =>
       console.log(`🚀 Server running on http://localhost:${process.env.PORT}`)
     );
   })
-  .catch(err => console.error('❌ MongoDB connection error:', err));
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
-  //
-  const userRoutes = require('./routes/userRoutes');
-app.use('/api/users', userRoutes);
+//
+const userRoutes = require("./routes/userRoutes");
+app.use("/api/users", userRoutes);
