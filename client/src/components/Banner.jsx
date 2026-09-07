821c40a186d33d25f4bb8ba37574c1d7098ac410
zainlatif <zainlatif702@gmail.com>
2026-09-07 16:56:13 +0500

    Remove jpg and add webp

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from 'react';
-import banner1 from '../assets/banner/banner1.png';
-import banner2 from '../assets/banner/banner2.png';
-import banner3 from '../assets/banner/banner3.png';
+import banner1 from '../assets/banner/banner1.webp';
+import banner2 from '../assets/banner/banner2.webp';
 import './Banner.css';
 
-const images = [banner1, banner2, banner3];
+const images = [banner1, banner2];
 
 const Banner = () => {
   const [index, setIndex] = useState(0);
